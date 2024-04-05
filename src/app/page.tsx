21b6337b1682a2a947c93f3b5b91b01be398ef33
Jayesh Chouhan <jayeshchouhan445@gmail.com>
2024-04-05 18:26:15 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <main className="  min-h-screen">
+      <div className=" ">
+        <Header />
+      </div>
+    </main>
+  );
+}
