a151fff7528c8df8abd3feba9daf1fd1ecacde00
Jayesh Chouhan <jayeshchouhan445@gmail.com>
2024-04-08 19:25:05 +0530

    added relational database using supabase and created graphql queries using stepzen

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import Header from "@/components/Header";
 export default function Home() {
   return (
     <main className="  min-h-screen">
+        <title>
+            Reddit-Clone
+        </title>
       <div className=" ">
         
       </div>
